642af5e01160b237d8d823cf72e29973b5e2ce6d
Alexander Kabanov <kabanov@live.com>
2025-03-23 11:01:04 +0400

    variable names have been changed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,15 +1,15 @@
 public class CreditPaymentService {
-    public double calculate(double amount, double months, double rate) {
+    public double calculate(double creditAmount, double months, double annualRate) {
         /*
-        Ежемесячный платёж = Коэффициент аннуитета(annuityRatio) * Сумма кредита(amount)
-        rate - годовая ставка.
+        Ежемесячный платёж = Коэффициент аннуитета(annuityRatio) * Сумма кредита(creditAmount)
+        annualRate - годовая ставка.
         Коэффициент аннуитета вычисляется по формуле:
         annuityRatio = i * (1 + i) ^ months / ((1 + i) ^ months - 1), где:
-        i — месячная процентная ставка (rate / 12 / 100);
+        monthlyInterestRate — месячная процентная ставка (rate / 12 / 100);
         months — количество месяцев, в течение которых выплачивается кредит.
         */
-        double i = rate / 12 / 100;
-        double annuityRatio = i * (Math.pow((1 + i), months) / (Math.pow((1 + i), months) - 1));
-        return annuityRatio * amount;
+        double monthlyInterestRate = annualRate / 12 / 100;
+        double annuityRatio = monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), months) / (Math.pow((1 + monthlyInterestRate), months) - 1));
+        return annuityRatio * creditAmount;
     }
 }
